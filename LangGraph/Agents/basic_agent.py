b49e4a0a3771d769fdbc4405fa958f23ba800c68
Abhilash Reddy Kothuru <kothuruabhilashreddy@gmail.com>
2026-03-17 15:20:48 -0400

    agent1 with gemini api

---
+++ LangGraph/Agents/basic_agent.py
@@ -0,0 +1,30 @@
+from typing import Dict, List, TypedDict, Union
+from langgraph.graph import StateGraph, START, END
+from langchain.messages import HumanMessage, AIMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+class AgentState(TypedDict):
+    message: List[Union[HumanMessage, AIMessage]]
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.7, max_tokens=2048, max_retries=0)
+
+def process_node(state: AgentState) -> AgentState:
+    reply = llm.invoke(state['message'])
+    state['message'].append(AIMessage(content=reply.content))
+    return state
+
+graph = StateGraph(AgentState)
+graph.add_node("process_node", process_node)
+graph.add_edge(START, "process_node")
+graph.add_edge("process_node", END)
+agent = graph.compile()
+
+input_message = input("Enter your message: ")
+reply = agent.invoke({"message": [HumanMessage(content= input_message)]})
+print("Final reply:", reply['message'][-1].content)
+
+
